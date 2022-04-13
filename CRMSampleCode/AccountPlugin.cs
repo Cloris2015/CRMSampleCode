46ae1fabe722b64e2a95f1d932878e6295d8e0c6
Cloris2015 <luckycloris@gmail.com>
2022-04-12 21:05:47 -0400

    initial checkin

---
+++ CRMSampleCode/AccountPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk;
+
+
+
+namespace CRMSampleCode
+{
+    public class AccountPlugin : IPlugin
+    {
+        void IPlugin.Execute(IServiceProvider serviceProvider)
+        {
+              throw new NotImplementedException();
+        }
+    }
+}
